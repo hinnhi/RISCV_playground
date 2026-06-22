f91d0d19fd5502141ee6638c9c1edd5f3ad39fa3
Michele Missiroli <163904638+hinnhi@users.noreply.github.com>
2026-06-22 23:16:50 +0200

    Create es3.s

---
+++ src/block/es3.s
@@ -0,0 +1,17 @@
+.global _start
+_start:
+	
+	ld a4,v
+	li a0,0
+	ld a5,8(a4)
+	ld a3,16(a4)
+	add a5,a5,a3
+	arli a5,a5,1
+	sd a5,0(a4)
+	
+/*
+translation into c language:
+
+	v[0] = ( v[1] + v[2])/2;
+	
+*/
