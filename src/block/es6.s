fc6c2c79d9c311d6aa5e112f5ceabe8be77b9b22
Michele Missiroli <163904638+hinnhi@users.noreply.github.com>
2026-06-23 00:11:28 +0200

    Create es6.s

---
+++ src/block/es6.s
@@ -0,0 +1,22 @@
+.global _start
+_start:
+
+	#int64_t x[4] = {1, 1, 0, 1};
+	#int64_t c = 0;
+	
+	la a4,x
+	ld a5,0(a4)
+	ld a2,8(a4)
+	ld a3,16(a4)
+	ld a4,24(a4)
+	sub a5,zero,a5
+	slli a5,a5,1
+	add a5,a5,a2
+	slli a5,a5,1
+	add a5,a5,a3
+	slli a5,a5,1
+	add a5,a5,a4
+	la t0,c
+	sd a5,0(t0)
+	
+	#c = -3;
