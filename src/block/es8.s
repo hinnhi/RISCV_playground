c7db40219b01a5fe12baca69319480023cfe4890
Michele Missiroli <163904638+hinnhi@users.noreply.github.com>
2026-06-23 01:37:01 +0200

    Create es8.s

---
+++ src/block/es8.s
@@ -0,0 +1,26 @@
+.global _start
+_start:
+	
+	mv a4,a2
+	mv a5,a3
+	add a5,a4,a5
+	mv a4,a1
+	bgtu a5,a4,.L2
+	mv a5,a1
+	mv a4,a5
+	mv a5,a3
+	add a5,a4,a5
+	mv a1,a5
+	
+.L2:
+	
+	
+#uint64_t v
+#
+#int main(){
+#	/* ... */
+#	if(a >= (b + c)) {
+#		a = a + c;
+#	}
+#	/* ... */
+#}
